ab2f063029856683a3512ab526f6d5c4d8bc2a34
Amhukerp <amamrest@gmail.com>
2026-06-10 16:11:46 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div
+      style={{
+        padding: "40px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>AMHUK Return & Exchange ERP</h1>
+
+      <p>Vercel Deployment Successful 🚀</p>
+    </div>
+  );
+}
+
+export default App;
